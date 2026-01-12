3d4cf45c8f2f97b7587389a8bf85ebb3a595f56b
Nivaldo Farias <badivia.f@gmail.com>
2026-01-12 14:03:22 -0300

    chore: add dotenv config to prisma config file

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,13 @@
-import "dotenv/config";
+import { existsSync } from "node:fs";
 
+import { config } from "dotenv";
 import { defineConfig, env } from "prisma/config";
+
+if (existsSync(".env")) {
+	config({ path: ".env" });
+} else {
+	config({ path: ".env.example" });
+}
 
 export default defineConfig({
 	schema: "prisma/schema.prisma",
